9e14313de5d1d33b78cd6f466350f9f27e69ce48
Aidan Rutter <solsane@gmail.com>
2018-01-19 10:50:44 -0500

    Prototypes and comments 2

--- lab2/lab2.ino
+++ lab2/lab2.ino
@@ -1,9 +1,46 @@
+/*
+ * Author: Aiden Rutter
+ * Lab 2: Thermostat
+ * COSC130 - SPRING2018
+ */
+
+//globals
+unsigned short m_desiredBCDTemp, m_currentBCDTemp;
+//prototypes
+/*
+ * Read: returns value of specified digit of packed BCD
+ * shift right 4*digit times
+*/
+int Read(short bcd, int digit);
+/*
+ * writes half a char, checks b/t 0 and 9 
+ */
+void Write(unsigned short &bcd, int digit, char value);
+/*
+ * Increments BCD value by one, if it's 9999 do nothing 
+ * Can use non binary arithmetic
+ */
+void Inc(unsigned short &bcd);
+/*
+ * Decrements BCD by one, do nothing if 0. 
+ * Can use non binary arithmetic
+ */
+void Dec(unsigned short &bcd);
+/*
+ * Returns BCD value as normal integer value
+ */
+int GetBCD(short bcd);
+/*
+ * Takes a normal int and encodes it into bcd format. If < 0, encode as 0, if > 9999, encode as 9999 
+ */
+void setBCD(short &bcd, int value);
+
 void setup() {
-  // put your setup code here, to run once:
+  
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
 }
